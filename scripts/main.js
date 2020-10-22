4fabd9cda52469884b0f4b44a4824b3822cd805b
moonchasers <moonchasers@users.noreply.github.com>
2020-10-22 22:04:45 +0100

    Duplication

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+
+$(function() {
+
+    // Populate using an object literal
+
+    // WTF.init({
+
+    //     heading: [
+    //         "Try this conversation",
+    //         "Give it a go with someone"
+    //     ],
+    //     response: [
+    //         "Help, I've run out of small talk",
+    //         "Not really feeling that one",
+    //         "I need a new one"
+    //     ],
+    //     template: [
+    //         "Big @color @animal",
+    //         "Silly @animal with @color fur"
+    //     ],
+    //     animal: [
+    //         "dog",
+    //         "cat",
+    //         "rabbit"
+    //     ],
+    //     color: [
+    //         "red",
+    //         "blue",
+    //         "green",
+    //         "yellow"
+    //     ]
+    // });
+
+    // Populate using a JSON file
+    // WTF.init( 'sample.json' );
+
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // @see https://support.google.com/drive/answer/37579?hl=en
+    WTF.init( '1hBZdqOyCb9BF6kao9lQO_ND80Z7x8G-4XGjVfl_cgfA' );
+
+});
